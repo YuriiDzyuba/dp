9d4f5d7b80cd57cce1d37cec306b3e74eb7d5cd7
YuriiDzyuba <iurii11022@gmail.com>
2022-01-09 12:52:49 +0200

    added profile.repository.ts

---
+++ api/src/profile/profile.repository.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@nestjs/common';
+import { ProfileType } from './types/profile.type';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { FollowEntity } from './entities/follow.entity';
+
+@Injectable()
+export class ProfileRepository {
+  constructor(
+    @InjectRepository(FollowEntity)
+    private readonly follow: Repository<FollowEntity>,
+  ) {}
+
+  async findOneFollowedProfile(
+    followerId: number,
+    followingId: number,
+  ): Promise<FollowEntity | undefined> {
+    return await this.follow.findOne({ followerId, followingId });
+  }
+
+  async followProfile(profileToFollow: FollowEntity): Promise<FollowEntity> {
+    return await this.follow.save(profileToFollow);
+  }
+
+  async unFollowProfile(
+    followerId: number,
+    followingId: number,
+  ): Promise<void> {
+    await this.follow.delete({ followerId, followingId });
+  }
+
+  buildProfileResponse(profile: ProfileType): ProfileType {
+    delete profile.email;
+    return profile;
+  }
+}
