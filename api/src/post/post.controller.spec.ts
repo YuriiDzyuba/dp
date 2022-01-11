b73771ceccceecb2f932005af400ba194ff2cbe4
YuriiDzyuba <iurii11022@gmail.com>
2022-01-11 16:19:13 +0200

    fixed post.controller.spec.ts

--- api/src/post/post.controller.spec.ts
+++ api/src/post/post.controller.spec.ts
@@ -15,37 +15,21 @@ import { UserModule } from '../user/user.module';
 import e from 'express';
 import { UpdatePostDto } from './dto/update-post.dto';
 import { createUniqueString } from '../utils/createUniqueString';
+import { PostRepository } from './post.repository';
+import { ProfileModule } from '../profile/profile.module';
+import { FileModule } from '../file/file.module';
+import { UserType } from '../user/types/user.type';
 
 describe('PostController', () => {
   let controller: PostController;
   let userService: UserService;
+  let savedUser: UserType;
 
-  const Location =
-    'https://first-my-test-01-bucket-00.s3.us-east-2.amazonaws.com/leverx/posts/rtert-xulpeq.jpeg';
-
-  const mockFileService = {
-    prepareImage: jest.fn(() => {
-      return 'verifiedImage';
-    }),
-    uploadNewImageToAWSs3: jest.fn(() => {
-      return { Location };
-    }),
-  };
+  const Location = 'https://first-mom/posts/rtert-xulpeq.jpeg';
 
   const mockImageFile = 'image.jpeg';
 
-  const mockUser: UserEntity = {
-    username: 'Andrey',
-    email: 'andrey44@mail.vgo',
-    id: 1,
-    password: 'qwerty',
-    summary: '',
-    avatar: '',
-    createdAt: new Date(),
-    posts: [],
-    likedPosts: [],
-    comments: [],
-  };
+  const mockBigImageFile = 'some big binary file';
 
   const mockNewUser: CreateUserDto = {
     username: `user-${createUniqueString()}`,
@@ -59,7 +43,7 @@ describe('PostController', () => {
     body: 'survived not only five ce, remaining etraset shesages, with',
     tagList: ['release', 'five', 'into'],
     imageFilter: 'multi',
-    image: '',
+    image: Location,
   };
 
   const mockPostToUpdate: UpdatePostDto = {
@@ -67,13 +51,23 @@ describe('PostController', () => {
     description: 'Updated',
     body: 'Updated',
     tagList: ['Updated', 'five', 'into'],
-    image: 'http//:Updated',
+    image: Location,
+  };
+
+  const mockFileService = {
+    prepareImage: jest.fn((image) => image),
+    uploadNewImageToAWSs3: jest.fn((verifiedImage, type) => ({
+      verifiedImage,
+      Location: verifiedImage === mockImageFile ? Location : undefined,
+    })),
   };
 
   beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [
         UserModule,
+        ProfileModule,
+        FileModule,
         TypeOrmModule.forFeature([UserEntity, PostEntity, FollowEntity]),
         TypeOrmModule.forRoot({
           ...testConfig,
@@ -81,7 +75,7 @@ describe('PostController', () => {
         }),
       ],
       controllers: [PostController],
-      providers: [PostService, FileService],
+      providers: [PostService, PostRepository],
     })
       .overrideProvider(FileService)
       .useValue(mockFileService)
@@ -89,7 +83,7 @@ describe('PostController', () => {
 
     controller = module.get<PostController>(PostController);
     userService = module.get<UserService>(UserService);
-    await userService.registerNewUser(mockNewUser);
+    savedUser = await userService.registerNewUser(mockNewUser);
   });
 
   afterAll(async () => {
@@ -105,30 +99,35 @@ describe('PostController', () => {
 
   it('should return new post ', async () => {
     expect(
-      await controller.createPost(mockUser, mockNewPost, mockImageFile as any),
-    ).toMatchObject({
-      title: 'It was popul 1960s ',
-      description: 'survived not ce',
-      body: 'survived not only five ce, remaining etraset shesages, with',
-      tagList: ['release', 'five', 'into'],
-      image: Location,
+      await controller.createPost(
+        savedUser as UserEntity,
+        mockNewPost,
+        mockImageFile as any,
+      ),
+    ).toEqual({
+      ...mockNewPost,
+      author: savedUser,
+      createdAt: expect.any(Date),
       favoriteCount: 0,
-      author: mockUser,
+      id: expect.any(Number),
     });
   });
 
-  it('should have properties id, createdAt', async () => {
-    expect(
-      await controller.createPost(mockUser, mockNewPost, mockImageFile as any),
-    ).toEqual(
-      expect.objectContaining({
-        id: expect.any(Number),
-        createdAt: expect.any(Date),
-      }),
-    );
+  it('should throw HttpException 503', async () => {
+    try {
+      expect(
+        await controller.createPost(
+          savedUser as UserEntity,
+          mockNewPost,
+          mockBigImageFile as any,
+        ),
+      ).toEqual({});
+    } catch (e) {
+      expect(e.message).toBe(`image size must be less than 3MB`);
+    }
   });
 
-  it('should update post', async () => {
+  it('should update and return updated post', async () => {
     expect(
       await controller.editPostById(
         1,
@@ -150,9 +149,12 @@ describe('PostController', () => {
   });
 
   it('should find posts by id', async () => {
-    expect(await controller.findOnePostsById(1)).toMatchObject(
-      mockPostToUpdate,
-    );
+    expect(await controller.findOnePostsById(1)).toEqual({
+      ...mockPostToUpdate,
+      createdAt: expect.any(Date),
+      favoriteCount: 0,
+      id: expect.any(Number),
+    });
   });
 
   it('should throw error', async () => {
@@ -164,16 +166,20 @@ describe('PostController', () => {
   });
 
   it('should like post', async () => {
-    expect(await controller.likePost(1, '1')).toMatchObject({
+    expect(await controller.likePost(1, '1')).toEqual({
       ...mockPostToUpdate,
+      createdAt: expect.any(Date),
       favoriteCount: 1,
+      id: expect.any(Number),
     });
   });
 
-  it('should like dislike post', async () => {
+  it('should dislike post', async () => {
     expect(await controller.disLikePost(1, '1')).toMatchObject({
       ...mockPostToUpdate,
+      createdAt: expect.any(Date),
       favoriteCount: 0,
+      id: expect.any(Number),
     });
   });
 
